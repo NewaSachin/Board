a98301972d5947ff77ebad7d9313903f9c79f50e
NewaSachin <sachinshrestha295@gmail.com>
2025-11-14 11:24:25 +0545

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import "./index.css";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,48 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react(),tailwindcss()],
+// // })
+
+
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react(), tailwindcss()],
+// //    base: "/",   
+
+// //   // IMPORTANT: Enable SPA fallback (fixes 404 on refresh)
+// //   server: {
+// //     historyApiFallback: true
+// //   },
+
+// //   // Optional but recommended for Vercel
+// //   build: {
+// //     outDir: "dist"
+// //   }
+// // })
+
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
-//    base: "/",   
 
-//   // IMPORTANT: Enable SPA fallback (fixes 404 on refresh)
-//   server: {
-//     historyApiFallback: true
-//   },
+//   // Required for correct routing on Vercel
+//   base: "/",
 
-//   // Optional but recommended for Vercel
 //   build: {
 //     outDir: "dist"
 //   }
 // })
-
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -35,11 +50,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  // Required for correct routing on Vercel
-  base: "/",
-
-  build: {
-    outDir: "dist"
-  }
+  base: '/',
+  build: { outDir: 'dist' },
 })
