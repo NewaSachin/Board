9e28034c00993df53e8fa91e5d8f657a42ca455c
NewaSachin <sachinshrestha295@gmail.com>
2025-11-14 11:15:12 +0545

    done

--- vite.config.js
+++ vite.config.js
@@ -8,20 +8,37 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//    base: "/",   
+
+//   // IMPORTANT: Enable SPA fallback (fixes 404 on refresh)
+//   server: {
+//     historyApiFallback: true
+//   },
+
+//   // Optional but recommended for Vercel
+//   build: {
+//     outDir: "dist"
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  // IMPORTANT: Enable SPA fallback (fixes 404 on refresh)
-  server: {
-    historyApiFallback: true
-  },
+  // Required for correct routing on Vercel
+  base: "/",
 
-  // Optional but recommended for Vercel
   build: {
     outDir: "dist"
   }
